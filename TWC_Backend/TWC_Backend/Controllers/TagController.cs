6a6e6e3bedff77049a46adbe6c6e5697511281f5
Brain-DedW <166624727+Brain-DedW@users.noreply.github.com>
2024-04-26 14:51:40 +0300

    Added all base tag operations into DBTagService

---
+++ TWC_Backend/TWC_Backend/Controllers/TagController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TWC_DatabaseLayer.DTOs;
+using TWC_DatabaseLayer.Models;
+using TWC_Services.DBService.Interfaces;
+using TWC_Services.DBService.Services;
+
+namespace TWC_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TagController : Controller
+    {
+        private IDBTagService _dbTagService;
+        public TagController(IDBTagService dBTagService)
+        {
+            _dbTagService = dBTagService;
+        }
+
+        [HttpPost]
+        [Route("AddTag")]
+        public async Task<ActionResult<Tag>> AddTag(string tagName)
+        {
+
+            Tag newTag = await _dbTagService.CreateTagAsync(tagName);
+
+            return Ok(newTag);
+        }
+        [HttpPut]
+        [Route("EditTag")]
+        public async Task<ActionResult<Tag>> EditTag(Tag newTag)
+        {
+
+            Tag editedTag = await _dbTagService.GetTagByIdAsync(newTag.Id);
+
+            return Ok(editedTag);
+        }
+    }
+}
---
+++ TWC_Backend/TWC_Services/DBService/Services/DBTagService.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TWC_DatabaseLayer;
+using TWC_DatabaseLayer.Models;
+using TWC_Services.DBService.Interfaces;
+
+namespace TWC_Services.DBService.Services
+{
+    public class DBTagService : IDBTagService
+    {
+        private DataContext _context;
+
+        public DBTagService(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<ProjectTag> AddTagToProjectAsync(Tag tag, Project project)
+        {
+            try
+            {
+                ProjectTag projectTag = new ProjectTag()
+                {
+                    TagId = tag.Id,
+                    ProjectId = project.Id,
+                    Tag = tag,
+                };
+
+                project.Tags.Add(projectTag);
+
+                await _context.SaveChangesAsync();
+                return projectTag;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Exception: {ex}");
+            }
+        }
+        public async Task<bool> RemoveTagFromProjectAsync(Tag tag, Project project)
+        {
+            try
+            {
+                var delTag = await _context.ProjectTags.SingleOrDefaultAsync(x => x.ProjectId == project.Id && x.TagId == tag.Id);
+
+                if (delTag == null)
+                {
+                    return false;
+                }
+
+                _context.ProjectTags.Remove(delTag);
+
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Exception: {ex}");
+            }
+        }
+
+        public async Task<UserTag> AddTagToUserAsync(Tag tag, User user)
+        {
+            try
+            {
+                UserTag userTag = new UserTag()
+                {
+                    TagId = tag.Id,
+                    UserId = user.Id,
+                    Tag = tag,
+                };
+
+                user.Tags.Add(userTag);
+
+                await _context.SaveChangesAsync();
+                return userTag;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Exception: {ex}");
+            }
+        }
+        public async Task<bool> RemoveTagFromUserAsync(Tag tag, User user)
+        {
+            try
+            {
+                var delTag = await _context.UserTags.SingleOrDefaultAsync(x => x.UserId == user.Id && x.TagId == tag.Id);
+
+                if (delTag == null)
+                {
+                    return false;
+                }
+
+                _context.UserTags.Remove(delTag);
+
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Exception: {ex}");
+            }
+        }
+        public async Task<Tag> CreateTagAsync(string tag)
+        {
+            try
+            {
+                Tag newTag = new Tag()
+                {
+                    Name = tag,
+                };
+                _context.Tags.Add(newTag);
+                await _context.SaveChangesAsync();
+                return newTag;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Exception: {ex}");
+            }
+        }
+
+        public async Task<bool> DeleteTagAsync(int id)
+        {
+            try
+            {
+                var tag = await _context.Tags.FindAsync(id);
+                if (tag == null)
+                {
+                    return false;
+                }
+
+                _context.Tags.Remove(tag);
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Exception: {ex}");
+            }
+        }
+
+        public async Task<Tag> EditTagAsync(Tag newTag)
+        {
+            try
+            {
+                var editedTag = await _context.Tags.FindAsync(newTag.Id);
+                if (editedTag == null)
+                {
+                    return null;
+                }
+
+                editedTag.Name = newTag.Name;
+                await _context.SaveChangesAsync();
+
+                return editedTag;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Exception: {ex}");
+            }
+        }
+
+        public async Task<List<Tag>> GetAllTagsAsync()
+        {
+            return await _context.Tags.ToListAsync();
+        }
+
+        public async Task<Tag> GetTagByIdAsync(int id)
+        {
+            try
+            {
+                var tag = await _context.Tags.FindAsync(id);
+                if (tag == null)
+                {
+                    return null;
+                }
+
+                return tag;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Exception: {ex}");
+            }
+        }
+
+        public async Task<Tag> GetTagByNameAsync(string name)
+        {
+            try
+            {
+                var tag = await _context.Tags.SingleOrDefaultAsync(t => t.Name == name);
+                if(tag == null)
+                {
+                    return null;
+                }
+                return tag;
+            }
+            catch(Exception ex) 
+            {
+                throw new Exception($"Exception: {ex}");
+            }
+        }
+
+        
+    }
+}
